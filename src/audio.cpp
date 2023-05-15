ff41fc48a619b763e5a7685686e7cc73c5cfc70c
intkr <inter_act@naver.com>
2023-05-15 22:03:58 +0900

    Audio system changes

--- src/audio.cpp
+++ src/audio.cpp
@@ -2,34 +2,42 @@
 
 Audio::Audio() {
 	fr = FMOD::System_Create(&fs);
-
 	fr = fs->init(512, FMOD_INIT_NORMAL, 0);
+	
+	FMOD::ChannelGroup* cg;
+	fs->createChannelGroup("bgm", &cg);
+	_sounds[_AUDIO_BGM] = cg;
+
+	fs->createChannelGroup("sfx", &cg);
+	_sounds[_AUDIO_SFX] = cg;
 }
 
 Audio::~Audio() {
-	FMOD::Sound* sound;
 
-	// Release all allocated audio data
-	for (auto a = _sounds.cbegin(); a != _sounds.cend();) {
-		a->second->getCurrentSound(&sound);
-		if (sound != NULL) {
-			sound->release();
-		}
-		a->second->stop();
-		_sounds.erase(a++);
+	// Release all allocated audio objects
+	stopBGM();
+
+	FMOD::Channel* channel;
+	FMOD::Sound* sound;
+	FMOD::ChannelGroup* cg = _sounds[_AUDIO_SFX];
+	int n;
+	cg->getNumChannels(&n);
+	for (; n; n--) {
+		cg->getChannel(n - 1, &channel);
+		channel->getCurrentSound(&sound);
+		sound->release();
 	}
 
 	fs->close();
 	fs->release();
 }
 
-bool Audio::addSound(std::string& path, const char* name, bool isLoop, bool isStream, int volume) {
+FMOD::Channel* Audio::addSound(std::string& path, const char* name, bool isLoop, bool isStream, AudioType type, int volume) {
 	std::string n = name;
-	return addSound(path, n, isLoop, isStream, volume);
+	return addSound(path, n, isLoop, isStream, type, volume);
 }
 
-bool Audio::addSound(std::string& path, std::string& name, bool isLoop, bool isStream, int volume) {
-
+FMOD::Channel* Audio::addSound(std::string& path, std::string& name, bool isLoop, bool isStream, AudioType type, int volume) {
 	// Setting audio boundaries
 	if (volume > 100) {
 		std::cout << "Warning: Sound \"" << name << "\" was set to volume " << volume << ", setting to 100 instead.\n";
@@ -42,72 +50,90 @@ bool Audio::addSound(std::string& path, std::string& name, bool isLoop, bool isS
 
 	// Create sound object
 	FMOD::Sound* sound;
-	fr = fs->createSound(path.c_str(), (isLoop ? FMOD_LOOP_NORMAL : FMOD_LOOP_OFF) | FMOD_2D | (isStream ? FMOD_CREATESTREAM : 0), NULL, &sound);
+	fr = fs->createSound(path.c_str(), (isLoop ? FMOD_LOOP_NORMAL : FMOD_LOOP_OFF) | FMOD_2D | (isStream ? FMOD_CREATESTREAM : 0), nullptr, &sound);
 	if (fr != FMOD_OK) {
-		return false;
+		return nullptr;
 	}
 
-	// Create channel object and pause sound immediately
+	// SFX should be played instantly, while BGM should be played by unpausing.
+	// This is because there might be a delay when loading BGMs onto a stream.
 	FMOD::Channel* channel;
-	fs->playSound(sound, NULL, 1, &channel);
+	fr = fs->playSound(sound, _sounds[type], true, &channel);
+	
 	if (fr != FMOD_OK) {
 		sound->release();
 		std::cout << "Sound \"" << name << "\" failed to play.\n";
-		return false;
+		return nullptr;
 	}
 	channel->setVolume((float)volume / 100);
-	_sounds.push_back(std::make_pair(name, channel));
-	return true;
+	//channel->setChannelGroup(_sounds[type]->getGroup());
+	return channel;
 }
 
-void Audio::stopSound(std::string& name) {
+void Audio::stopBGM() {
+	FMOD::Channel* channel;
 	FMOD::Sound* sound;
-	
+	int n;
+
 	// Release all sounds if 'name' is empty
-	if (name == "") {
-		for (auto a = _sounds.cbegin(); a != _sounds.cend();) {
-			a->second->getCurrentSound(&sound);
-			sound->release();
-			a++;
-		}
-		return;
+	FMOD::ChannelGroup* cg = _sounds[_AUDIO_BGM];
+	cg->getNumChannels(&n);
+	for (; n; n--) {
+		cg->getChannel(n - 1, &channel);
+		channel->getCurrentSound(&sound);
+		sound->release();
 	}
+	return;
+}
+
+void Audio::stopBGM(std::string& name, AudioType type) {
+	FMOD::Channel* channel;
+	FMOD::Sound* sound;
 
 	// if not empty, release sound from corresponding channel
-	FMOD::Channel* ch = getChannel(name);
-	if (ch == nullptr) {
+	//channel = getChannelByName(name, type);
+	if (bgmChannels.count(name) == 0) {
 		std::cout << "Sound name \"" << name << "\" doesn't exist, failed to delete.\n";
 		return;
 	}
-	
-	ch->getCurrentSound(&sound);
+
+	channel = bgmChannels[name];
+	channel->getCurrentSound(&sound);
 	sound->release();
 }
 
-FMOD::Channel* Audio::getChannel(std::string& name) {
-	for (int i = 0; i < _sounds.size(); i++) {
-		if (_sounds[i].first == name) return _sounds[i].second;
-	}
+FMOD::Channel* Audio::getBGMChannel(std::string& name) {
+	FMOD::Channel* ch;
+	//ch = _sounds[type]->getChannel(name);
 
-	std::cout << "Sound name \"" << name << "\" doesn't exist, failed to get channel.\n";
-	return NULL;
+	if (bgmChannels.count(name) == 0) {
+		std::cout << "Sound name \"" << name << "\" doesn't exist, failed to get channel.\n";
+	}
+	ch = bgmChannels[name];
+	return ch;
 }
 
 void Audio::update() {
 	FMOD::Sound* sound;
+	FMOD::ChannelGroup* cg;
+	FMOD::Channel* ch;
 	bool b;
-	for (auto a = _sounds.begin(); a != _sounds.end();) {
-		fr = a->second->isPlaying(&b);
+	int i;
 
-		if (!b || fr == FMOD_ERR_INVALID_HANDLE) {
-			a->second->getCurrentSound(&sound);
-			sound->release();
-			//a->second->stop(); // this should be unnecessary
-			a = _sounds.erase(a);
+	for (auto it = _sounds.begin(); it != _sounds.end();) {
+		cg = it->second;
+
+		for (cg->getNumChannels(&i); i; i--) {
+			cg->getChannel(i, &ch);
+			fr = ch->isPlaying(&b);
+
+			if (!b || fr == FMOD_ERR_INVALID_HANDLE) {
+				ch->getCurrentSound(&sound);
+				sound->release();
+			}
 		}
-		else {
-			a++;
-		}
+
+		it++;
 	}
 }
 
--- src/audio.h
+++ src/audio.h
@@ -1,5 +1,5 @@
 #pragma once
-#include <vector>
+#include <map>
 #include <string>
 #include <iostream>
 
@@ -7,12 +7,17 @@
 #include "fmod_output.h"
 #include "fmod_errors.h"
 
+// random note : Channels are (presumably) released automatically when its Sound is released
+
+enum AudioType { _AUDIO_BGM = 1, _AUDIO_SFX = 2
+};
+
 class Audio {
 public:
 	Audio();
 	~Audio();
 
-	FMOD::Channel* getChannel(std::string& name);
+	FMOD::Channel* getBGMChannel(std::string& name);
 
 	// Creates a Sound object.
 	//
@@ -20,16 +25,18 @@ public:
 	// name : Audio object identifier.
 	// isLoop : Decides if audio loops indefinitely.
 	// isStream : Decides if audio should be played through a stream. Set this to true for large audio files.
+	// type : ChannelGroup identifier for _sounds. ("bgm", "sfx")
 	// volume : Volume of audio (0~100).
-	bool addSound(std::string& path, const char* name, bool isLoop, bool isStream, int volume);
-	bool addSound(std::string& path, std::string& name, bool isLoop, bool isStream, int volume);
+	FMOD::Channel* addSound(std::string& path, const char* name, bool isLoop, bool isStream, AudioType type, int volume);
+	FMOD::Channel* addSound(std::string& path, std::string& name, bool isLoop, bool isStream, AudioType type, int volume);
 
-	// Checks through all allocated audio data and releases all that finished playing.
+	// Checks through all allocated audio objects and releases all that finished playing.
 	void update();
 
 	// Releases the sound with identifier 'name'.
-	// If name is empty (""), stop all sounds.
-	void stopSound(std::string& name);
+	// If there's no parameter, stop all sounds.
+	void stopBGM();
+	void stopBGM(std::string& name, AudioType type);
 
 	// Returns true if there are any items in _sounds.
 	// Returns false otherwise.
@@ -39,7 +46,11 @@ private:
 	FMOD::System* fs;
 	FMOD_RESULT fr;
 
-	std::vector<std::pair<std::string, FMOD::Channel*>> _sounds;
+	// "sfx" and "bgm" for now, divide this later for advanced adjustments
+	std::map<AudioType, FMOD::ChannelGroup*> _sounds;
+
+	// used for _sounds["bgm"]
+	std::map<std::string, FMOD::Channel*> bgmChannels;
 };
 
 /*
