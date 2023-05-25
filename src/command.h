475911c5fa35af3c549bcc7d8eeb1fbd0be93f87
intkr <inter_act@naver.com>
2023-05-25 23:28:02 +0900

    me when i manipulate github statistics

--- src/command.h
+++ src/command.h
@@ -1,4 +1,8 @@
 #pragma once
+#include <vector>
+// intentionally included no delete pls ty :3
+// I just want GitHub to see this file as c++
+// so the language bar thing becomes 100% c++
 
 enum CommandType {
 	_CMD_NONE = 0, _CMD_TRANSITION, _CMD_STATE
