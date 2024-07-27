8f08860a35db24bc726c9a931e4430b865c1c48d
intkr <91324649+intkr@users.noreply.github.com>
2024-07-27 23:48:03 +0900

    Writing notes to organize thoughts

--- src/main.cpp
+++ src/main.cpp
@@ -22,6 +22,95 @@ INT WINAPI WinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _
 }
 
 /*
+Overview of how the object-component stuff work
+
+	Example object hierarchy:
+
+	Object
+		-> Entity
+			-> UI
+			-> Player/Enemy
+			etc
+		-> Zone
+			-> Loading Zone
+		-> Prop
+			-> Effects
+			-> Tooltip
+			-> Particle
+		etc
+
+
+	Brief documentation on classes and member variables:
+	(Classes are capitalized and variables aren't)
+
+	- Object
+		- current state (walking, shooting, etc)
+		- messaging system to communicate between components
+		- geometric data (position, size, rotation, etc)
+		
+		- Physics component
+		  : Handles the overall movement of an object.
+
+		- State component
+		  : Handles the current state an object is in.
+		    This affects a 'state' variable owned by the object itself,
+			which in turn affects all other component that are based on its value.
+			- functions to call upon state change (maybe just messages?)
+
+		- Graphics component
+		  : Handles all visual representation of an object.
+		    update(ms) updates Source and Target,
+			render(Renderer*) passes Source and Target data to Renderer's render().
+			* An object would have a vector of Graphics components for every sprite required.
+
+			- Source
+			  : Handles what sprite to use from a spritesheet image,
+			    and its animation sequences.
+				- texture data (SDL_Texture*)
+				- texture box (to get a specific portion of a spritesheet)
+				- animation sequences (for any visual sprite changes)
+			- Target
+			  : Handles where to draw the sprite (position, size, rotation etc)
+			    and its motion sequences.
+				- motion sequences (for any movement within the object)
+				- render-related boxes (relative to the object's position)
+				- reference to the object's transform (to calculate the actual rendering box)
+				- texture modifiers (hue, brightness, etc)
+
+		- Collision component
+		  : Contains all collision-related data of an object.
+			* Collision checks should be done from outside entities or its components.
+
+			- motion sequences (for any collision movement within the object)
+			- collision data (bounding box, actual collision shape, etc)
+			- callback data (maybe a map<colliding object type, message to send / function to call>)
+			- object type? (idk not sure)
+
+		- Input component
+		  : Handles all incoming inputs and callbacks set to an object.
+			- callback data (map<input action, function>)
+			- current input state (input actions and its type value - check input_event.h)
+
+	- Motion
+	  : Handles all changes of an object's geometric data.
+
+		- attributes (active, name, length?, etc)
+		- list of motion parts
+		- reference to the source / target box
+		  (motion calculations are done with the source box data,
+		   and its results are saved on the target box)
+	- Motion Parts (temporary name)
+
+	Overview of the game logic flow:
+		- Process input
+		- Update object graphics (source, target)
+		- Check for collisions
+			- Gather relevant collision data into a vector (collider, collidee)
+			- Iterate through the vector and do stuff accordingly
+		- Render entities
+*/
+
+/*
 Stuff to do when I have my IDE at home:
 	- fix all the existing syntax errors / inconsistencies
 
@@ -55,7 +144,7 @@ Highly prioritized stuff I must do before I eventually meltdown:
 
 	- Implement attributes for sprites / entities / states
 	  Notable examples I should add:
-		- StateSpecific (Entity, Sprite) : used for resources specific to one state only (allows smooth state transitions if false)
+		- StateSpecific (object, Sprite) : used for resources specific to one state only (allows smooth state transitions if false)
 		- Interactable (State) : false for cutscenes and whatnot
 		- Paused (State) : self-explanatory
 */
@@ -91,7 +180,7 @@ Not-too-important small notes for the brainrot future me:
 
 	- BPM should be handled by a music-specific class object, not Channels or whatever.
 
-	- When checking for collision of an entity, consider overloading a single collision-checking function
+	- When checking for collision of an object, consider overloading a single collision-checking function
 	  with all possible targets - a single point (mouse cursor), a line, convex/concave shapes, etc.
 */
 
