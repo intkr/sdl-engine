25de893292fa94bfe3b95e35aade1ecc3152b07e
intkr <91324649+intkr@users.noreply.github.com>
2024-08-09 23:48:03 +0900

    more types of motion frames

--- src/motion_frame.cpp
+++ src/motion_frame.cpp
@@ -14,13 +14,97 @@ void MotionFrame::updateTime(ms& delta) {
 }
 
 SDL_FRect Motions::SineWave2D::apply(const SDL_FRect& sourceBox) {
-	SDL_FRect box = sourceBox;
-
-	double distance = amplitude * sin(frequency * PI * ((double)elapsedTime.count() / MS_PER_SECOND));
-
-	box.x += (float)(distance * cos(angle_rad));
-	box.y += (float)(distance * sin(angle_rad) * -1);
-
-	return box;
+	SDL_FPoint sourcePoint = getBoxCenter(sourceBox);
+	SDL_FRect newBox = setBoxPosition(sourceBox, apply(sourcePoint));
+	return newBox;
 }
 
+SDL_FPoint Motions::SineWave2D::apply(const SDL_FPoint& sourcePoint) {
+	SDL_FPoint point = sourcePoint;
+
+	float distance = amplitude * sin(frequency * PI * ((float)elapsedTime.count() / MS_PER_SECOND));
+
+	point.x += distance * cos(angle_rad);
+	point.y += distance * sin(angle_rad) * -1;
+
+	return point;
+}
+
+SDL_FRect Motions::Move2D_Linear::apply(const SDL_FRect& sourceBox) {
+	SDL_FPoint sourcePoint = getBoxCenter(sourceBox);
+	SDL_FRect newBox = setBoxPosition(sourceBox, apply(sourcePoint));
+	return newBox;
+}
+
+SDL_FPoint Motions::Move2D_Linear::apply(const SDL_FPoint& sourcePoint) {
+	SDL_FPoint point;
+	float percentage = (float)elapsedTime.count() / duration.count();
+	point = start + (end - start) * percentage;
+	return point;
+}
+
+SDL_FRect Motions::Move2D_EaseSine::apply(const SDL_FRect& sourceBox) {
+	SDL_FPoint sourcePoint = getBoxCenter(sourceBox);
+	SDL_FRect newBox = setBoxPosition(sourceBox, apply(sourcePoint));
+	return newBox;
+}
+
+SDL_FPoint Motions::Move2D_EaseSine::apply(const SDL_FPoint& sourcePoint) {
+	SDL_FPoint point;
+	float percentage = (float)elapsedTime.count() / duration.count();
+	float sineValue;
+	switch (mode) {
+	case 1: // In
+		sineValue = 1 - cos(percentage * PI / 2);
+		break;
+
+	case 2: // Out
+		sineValue = sin(percentage * PI / 2);
+		break;
+
+	case 3: // Both
+		sineValue = -(cos(percentage * PI) - 1) / 2;
+		break;
+
+	case 0: // None
+	default: // (Shouldn't be used)
+		sineValue = percentage;
+		break;
+	}
+
+	point = start + (end - start) * sineValue;
+	return point;
+}
+
+SDL_FRect Motions::Move2D_EaseBack::apply(const SDL_FRect& sourceBox) {
+	SDL_FPoint sourcePoint = getBoxCenter(sourceBox);
+	SDL_FRect newBox = setBoxPosition(sourceBox, apply(sourcePoint));
+	return newBox;
+}
+
+SDL_FPoint Motions::Move2D_EaseBack::apply(const SDL_FPoint& sourcePoint) {
+	SDL_FPoint point;
+	float percentage = (float)elapsedTime.count() / duration.count();
+	float sineValue;
+	switch (mode) {
+	case 1: // In
+		sineValue = 1 - cos(percentage * PI / 2);
+		break;
+
+	case 2: // Out
+		sineValue = sin(percentage * PI / 2);
+		break;
+
+	case 3: // Both
+		sineValue = -(cos(percentage * PI) - 1) / 2;
+		break;
+
+	case 0: // None
+	default: // (Shouldn't be used)
+		sineValue = percentage;
+		break;
+	}
+
+	point = start + (end - start) * sineValue;
+	return point;
+}
--- src/motion_frame.h
+++ src/motion_frame.h
@@ -7,7 +7,7 @@
 #include "exception.h"
 
 // Commonly used constants
-#define PI 3.14159265358979323846
+#define PI 3.14159265358979323846f
 #define MS_PER_SECOND 1000
 
 // Base class for all types of motion frames.
@@ -17,36 +17,143 @@ public:
 
 	void reset() { elapsedTime = ms(0); }
 	void updateTime(ms& delta);
+
+	// Signals the frame its beginning, storing the initial position internally.
+	// This is done for easier calculations in point-to-point movement, especially targeted ones.
+	virtual void begin(const SDL_FPoint& point);
+	virtual void begin(const SDL_FRect& box);
+
+	// Calculates and returns the new values based on the parameter.
 	virtual SDL_FRect apply(const SDL_FRect& sourceBox) = 0;
+	virtual SDL_FPoint apply(const SDL_FPoint& sourcePoint) = 0;
 
 protected:
 	ms elapsedTime;
 	ms duration;
 };
 
+// Reference used for motion calculations:
+/*
+https://gizma.com/easing/
+*/ 
+
 namespace Motions {
+	SDL_FRect setBoxPosition(const SDL_FRect& box, const SDL_FPoint& newPos) {
+		SDL_FRect newBox = box;
+		newBox.x = newPos.x, newBox.y = newPos.y;
+		return newBox;
+	}
+
+	SDL_FPoint getBoxCenter(const SDL_FRect& box) {
+		SDL_FPoint point = { box.x + box.w / 2, box.y  + box.h / 2 };
+		return point;
+	}
+
 	/*
-	Simulates a sine function in a rotated axis.
-	Equal to the mathematical function "amplitude * sin(frequency*πt)",
-	where t = elapsed time (in seconds).
+	The object moves in a sine function on a predetermined, rotated axis.
 
 	Parameters
-	- amplitude : The size of the sine wave.
-	- frequency : The frequency of each sine wave.
+	- amplitude : The size of the sine wave, in pixels.
+	- length : The time to cycle through one sine wave, in seconds.
 	- angle_deg : The angle of the sine wave, in degrees.
 	*/
 	class SineWave2D : public MotionFrame {
 	public:
-		SineWave2D(ms duration, double amplitude, double frequency, double angle_deg) :
-		  MotionFrame(duration), amp(amplitude), freq(frequency) {
-			angle_rad = remainder(angle_deg, 360.0) * 180.0 / PI;
+		SineWave2D(ms duration, float amplitude, float length, float angle_deg) :
+		  MotionFrame(duration), amp(amplitude), freq(2 * PI / length),
+		  angle_rad(remainder(angle_deg, 360.0) * 180.0 / PI) {}
+
+		SDL_FRect apply(const SDL_FRect& sourceBox) override;
+		SDL_FPoint apply(const SDL_FPoint& sourcePoint) override;
+
+	private:
+		float amp;
+		float freq;
+		float angle_rad;
+	}
+
+	/*
+	The object moves to a target position, linearly.
+
+	Parameters
+	- target : The desired end position of the object.
+	*/
+	class Move2D_Linear : public MotionFrame {
+	public:
+		Move2D_Linear(ms duration, SDL_FPoint& target) :
+		  MotionFrame(duration), end(target) {}
+		
+		void begin(const SDL_FPoint& point) override {
+			start = point;
+		}
+		void begin(const SDL_FRect& box) override {
+			start = SDL_FPoint{ box.x, box.y };
 		}
 
 		SDL_FRect apply(const SDL_FRect& sourceBox) override;
+		SDL_FPoint apply(const SDL_FPoint& sourcePoint) override;
 
 	private:
-		double amp;
-		double freq;
-		double angle_rad;
+		SDL_FPoint start;
+		SDL_FPoint end;
+	}
+
+	/*
+	The object moves to a target position, with easing sine functions.
+
+	Parameters
+	- target : The desired end position of the object.
+	- easeIn / easeOut : Whether or not to add easing effects.
+	*/
+	class Move2D_EaseSine : public MotionFrame {
+	public:
+		Move2D_EaseSine(ms duration, SDL_FPoint& target, bool easeIn, bool easeOut) :
+		  MotionFrame(duration), end(target), mode(easeIn + easeOut * 2) {}
+		
+		void begin(const SDL_FPoint& point) override {
+			start = point;
+		}
+		void begin(const SDL_FRect& box) override {
+			start = SDL_FPoint{ box.x, box.y };
+		}
+
+		SDL_FRect apply(const SDL_FRect& sourceBox) override;
+		SDL_FPoint apply(const SDL_FPoint& sourcePoint) override;
+
+	private:
+		SDL_FPoint start;
+		SDL_FPoint end;
+		// Values: None = 0, In, Out, Both
+		short mode;
+	}
+
+	/*
+	The object moves to a target position, overshooting a small amount and easing back.
+
+	Parameters
+	- target : The desired end position of the object.
+	- easeIn / easeOut : Whether or not to ease back.
+	*/
+	class Move2D_EaseBack : public MotionFrame {
+	public:
+	// unfinished
+		Move2D_EaseBack(ms duration, SDL_FPoint& target, bool easeIn, bool easeOut) :
+		  MotionFrame(duration), end(target), mode(easeIn + easeOut * 2) {}
+		
+		void begin(const SDL_FPoint& point) override {
+			start = point;
+		}
+		void begin(const SDL_FRect& box) override {
+			start = SDL_FPoint{ box.x, box.y };
+		}
+
+		SDL_FRect apply(const SDL_FRect& sourceBox) override;
+		SDL_FPoint apply(const SDL_FPoint& sourcePoint) override;
+
+	private:
+		SDL_FPoint start;
+		SDL_FPoint end;
+		// Values: None = 0, In, Out, Both
+		short mode;
 	}
 }
